07726ab3a5ccd7f9780fcc4f100988f9c7ef27a0
Sai Satwika Balivada <74233335+sai-satwika@users.noreply.github.com>
2022-10-24 19:58:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ def knn():
 def ensemble():
     return render_template('ensemble.html')
 
-# ...
-@app.route("/predict", methods = ['POST', 'GET'])
-def predict():
+# ...SVM
+@app.route("/svmpredict", methods = ['POST', 'GET'])
+def svmpredict():
     try:
         if request.method == 'POST':
             to_predict_dict = request.form.to_dict()
@@ -40,7 +40,79 @@ def predict():
             values = to_predict_list
             print("form", to_predict_dict)
             print("values", values)
-            model = pickle.load(open('heart.pkl', 'rb'))
+            model = pickle.load(open('models/svm.pkl', 'rb'))
+            values = np.asarray(values)
+            pred = model.predict(values.reshape(1, -1))[0]
+            print("ans", pred)
+            # return ans
+            # print("pred", pred)
+            
+    except:
+        message = "Please enter valid Data"
+        return render_template("home.html", message = message)
+
+    return render_template('predict.html', pred = pred)
+
+# ...ANN
+@app.route("/annpredict", methods = ['POST', 'GET'])
+def annpredict():
+    try:
+        if request.method == 'POST':
+            to_predict_dict = request.form.to_dict()
+            to_predict_list = list(map(float, list(to_predict_dict.values())))
+            # pred = predict(to_predict_list, to_predict_dict)
+            values = to_predict_list
+            print("form", to_predict_dict)
+            print("values", values)
+            model = pickle.load(open('models/ann.pkl', 'rb'))
+            values = np.asarray(values)
+            pred = model.predict(values.reshape(1, -1))[0]
+            print("ans", pred)
+            # return ans
+            # print("pred", pred)
+            
+    except:
+        message = "Please enter valid Data"
+        return render_template("home.html", message = message)
+
+    return render_template('predict.html', pred = pred)
+
+# ...KNN
+@app.route("/knnpredict", methods = ['POST', 'GET'])
+def knnpredict():
+    try:
+        if request.method == 'POST':
+            to_predict_dict = request.form.to_dict()
+            to_predict_list = list(map(float, list(to_predict_dict.values())))
+            # pred = predict(to_predict_list, to_predict_dict)
+            values = to_predict_list
+            print("form", to_predict_dict)
+            print("values", values)
+            model = pickle.load(open('models/knn.pkl', 'rb'))
+            values = np.asarray(values)
+            pred = model.predict(values.reshape(1, -1))[0]
+            print("ans", pred)
+            # return ans
+            # print("pred", pred)
+            
+    except:
+        message = "Please enter valid Data"
+        return render_template("home.html", message = message)
+
+    return render_template('predict.html', pred = pred)
+
+# ...Ensemble
+@app.route("/ensemblepredict", methods = ['POST', 'GET'])
+def ensemblepredict():
+    try:
+        if request.method == 'POST':
+            to_predict_dict = request.form.to_dict()
+            to_predict_list = list(map(float, list(to_predict_dict.values())))
+            # pred = predict(to_predict_list, to_predict_dict)
+            values = to_predict_list
+            print("form", to_predict_dict)
+            print("values", values)
+            model = pickle.load(open('models/ensemble.pkl', 'rb'))
             values = np.asarray(values)
             pred = model.predict(values.reshape(1, -1))[0]
             print("ans", pred)
